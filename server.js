6a5379076beeea5f1b98734f3afb906b41bf24bb
Amey R <amey@cyperts.net>
2018-06-04 15:52:04 +0530

    Changes for Heroku

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.get('/*', function(req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+// app.use(express.static(__dirname + "/dist"));
 app.listen(process.env.PORT || 8080);
