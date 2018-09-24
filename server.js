c7f2140de6d75f3e658a7ba2d06af16145885890
AmeyRCyperts <39903808+AmeyRCyperts@users.noreply.github.com>
2018-09-24 18:49:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-const express = require("express");
+const express = require('express');
 const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist/demoApp'));
+// Start the app by listening on the default
 
-app.use(express.static(__dirname + "/dist"));
+// app.listen(process.env.PORT || 8080);
+var server_port = process.env.server_port || 80
+var server_ip_address = process.env.server_port || '0.0.0.0'
 
-app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+app.listen(server_port, server_ip_address, function () {  
+        console.log( "Listening on " + server_ip_address + ", port " + server_p$
 });
-app.listen(process.env.PORT || 8080);
